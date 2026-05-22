ac32bd5473411578609c157088ec88344d24d311
S.M. Mostofa Morshed Sayeem <198875870+smmmsmo@users.noreply.github.com>
2026-05-22 18:30:27 +0600

    Remove unused ArrayPrint class and main.java; add Q1 and Q2 classes for merging arrays with test files

--- Array/array_java/Main.java
+++ Array/array_java/Main.java
@@ -9,89 +9,87 @@ import utils.ArrayUtility;
 
 public class Main {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        ArrayManager manager
-                = new ArrayManager(new int[]{5, 6, 7, 8, 9});
+                ArrayManager manager = new ArrayManager(new int[] { 5, 6, 7, 8, 9 });
 
-        ArrayUtility.printTitle("Initial Array");
+                ArrayUtility.printTitle("Initial Array");
 
-        TraverseOperation.forward(manager.getArray());
+                TraverseOperation.forward(manager.getArray());
 
-        ArrayUtility.printTitle("Insert Operations");
+                ArrayUtility.printTitle("Insert Operations");
 
-        manager.setArray(
-                InsertOperation.atBeginning(
-                        manager.getArray(),
-                        100));
+                manager.setArray(
+                                InsertOperation.atBeginning(
+                                                manager.getArray(),
+                                                100));
 
-        manager.setArray(
-                InsertOperation.atPosition(
-                        manager.getArray(),
-                        200,
-                        3));
+                manager.setArray(
+                                InsertOperation.atPosition(
+                                                manager.getArray(),
+                                                200,
+                                                3));
 
-        manager.setArray(
-                InsertOperation.atEnd(
-                        manager.getArray(),
-                        300));
+                manager.setArray(
+                                InsertOperation.atEnd(
+                                                manager.getArray(),
+                                                300));
 
-        ArrayUtility.printTitle("Modify Operations");
+                ArrayUtility.printTitle("Modify Operations");
 
-        ModifyOperation.update(
-                manager.getArray(),
-                0,
-                111);
+                ModifyOperation.update(
+                                manager.getArray(),
+                                0,
+                                111);
 
-        ModifyOperation.update(
-                manager.getArray(),
-                2,
-                222);
+                ModifyOperation.update(
+                                manager.getArray(),
+                                2,
+                                222);
 
-        ModifyOperation.update(
-                manager.getArray(),
-                manager.getArray().length - 1,
-                333);
+                ModifyOperation.update(
+                                manager.getArray(),
+                                manager.getArray().length - 1,
+                                333);
 
-        ArrayUtility.printTitle("Traversal");
+                ArrayUtility.printTitle("Traversal");
 
-        System.out.println("Forward Traversal:");
-        TraverseOperation.forward(manager.getArray());
+                System.out.println("Forward Traversal:");
+                TraverseOperation.forward(manager.getArray());
 
-        System.out.println("\nReverse Traversal:");
-        TraverseOperation.reverse(manager.getArray());
+                System.out.println("\nReverse Traversal:");
+                TraverseOperation.reverse(manager.getArray());
 
-        ArrayUtility.printTitle("Search");
+                ArrayUtility.printTitle("Search");
 
-        System.out.print("array -> ");
-        ArrayUtility.printArray(manager.getArray());
-        int index
-                = SearchOperation.linearSearch(
-                        manager.getArray(),
-                        222);
+                System.out.print("array -> ");
+                ArrayUtility.printArray(manager.getArray());
+                int index = SearchOperation.linearSearch(
+                                manager.getArray(),
+                                222);
 
-        if (index != -1) {
-            System.out.println("Target found at index : " + index);
-        } else {
-            System.out.println("Target not found!");
+                if (index != -1) {
+                        System.out.println("Target found at index : " + index);
+                } else {
+                        System.out.println("Target not found!");
+                }
+
+                ArrayUtility.printTitle("Delete Operations");
+
+                manager.setArray(
+                                DeleteOperation.atBeginning(
+                                                manager.getArray()));
+
+                manager.setArray(
+                                DeleteOperation.atPosition(
+                                                manager.getArray(),
+                                                2));
+
+                manager.setArray(
+                                DeleteOperation.atEnd(
+                                                manager.getArray()));
+
+                System.out.println("Final Array:");
+                TraverseOperation.forward(manager.getArray());
         }
-
-        ArrayUtility.printTitle("Delete Operations");
-
-        manager.setArray(
-                DeleteOperation.atBeginning(
-                        manager.getArray()));
-
-        manager.setArray(
-                DeleteOperation.atPosition(
-                        manager.getArray(),
-                        2));
-
-        manager.setArray(
-                DeleteOperation.atEnd(
-                        manager.getArray()));
-
-        System.out.println("Final Array:");
-        TraverseOperation.forward(manager.getArray());
-    }
 }
